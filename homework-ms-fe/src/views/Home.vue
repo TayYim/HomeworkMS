43771d353b37d7db2b707d77de32ec4370431c99
yan_sy <yan_sy@126.com>
2018-10-17 15:27:05 +0800

    add FunctionButton to home page

--- homework-ms-fe/src/views/Home.vue
+++ homework-ms-fe/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <management />
+    <h1>RABC demo</h1>
+    <function-button button-name="作业提交" p-i-d-required="1"></function-button>
+    <function-button button-name="作业点评" p-i-d-required="2"></function-button>
+    <function-button button-name="成绩统计" p-i-d-required="3"></function-button>
+    <function-button button-name="系统管理" p-i-d-required="4"></function-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import Management from '@/components/Management.vue'
+import FunctionButton from '@/components/FunctionButton.vue'
 
 export default {
   name: 'home',
   components: {
-    Management
-  }
+    FunctionButton
+  },
+  mounted() {
+    console.log('mounted');
+    this.$store.commit('setUser','2001')
+  },
 }
 </script>
