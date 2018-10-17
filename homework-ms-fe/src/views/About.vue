39d241d36b5acc4cca39c626ed5cf3b5b94352e2
yan_sy <yan_sy@126.com>
2018-10-17 17:08:31 +0800

    add github url

--- homework-ms-fe/src/views/About.vue
+++ homework-ms-fe/src/views/About.vue
@@ -3,5 +3,6 @@
     <h1>Homeword for SSAD</h1>
     <h2>XJTU SE</h2>
     <h3>Tay 2018.10.17</h3>
+    <h3>GitHub: <a href="https://github.com/TayYin/HomeworkMS">https://github.com/TayYin/HomeworkMS</a></h3>
   </div>
 </template>
