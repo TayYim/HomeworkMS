991a2d0837caec10da3eb8806cf74f1ae728345e
yan_sy <yan_sy@126.com>
2018-10-17 15:59:22 +0800

    add commit mutation after selecting username

--- homework-ms-fe/src/views/Home.vue
+++ homework-ms-fe/src/views/Home.vue
@@ -1,47 +1,49 @@
 <template>
   <div class="home">
-    <h1>RABC demo</h1>
-    <select @change="onUserChange" v-model="selectedUsername" id="username">
+    <h1>RABC0 demo</h1>
+    <div>
+      选择用户:
+      <select @change="onUserChange" v-model="selectedUsername" id="username">
       <option v-for="user in RBAC.User" :key="user.UID" :value="user.UID">
         {{user.name}}
       </option>
     </select>
+    </div>
     <h2>我是{{user.name}}，角色为{{user.roleNames}}</h2>
-    <function-button button-name="作业提交" p-i-d-required="1"></function-button>
+    <div class="button-field">
+      <function-button button-name="作业提交" p-i-d-required="1"></function-button>
     <function-button button-name="作业点评" p-i-d-required="2"></function-button>
     <function-button button-name="成绩统计" p-i-d-required="3"></function-button>
     <function-button button-name="系统管理" p-i-d-required="4"></function-button>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import FunctionButton from '@/components/FunctionButton.vue'
+import FunctionButton from "@/components/FunctionButton.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     FunctionButton
   },
   mounted() {
-    console.log('mounted');
-    this.$store.commit('setUser','1001')
+    console.log("mounted");
+    this.$store.commit("setUser", "1001");
   },
-  computed:{
-    ...mapState([
-      'user',
-      'RBAC'
-    ])
+  computed: {
+    ...mapState(["user", "RBAC"])
   },
-  data(){
+  data() {
     return {
-      selectedUsername: '二狗子'
-    }
+      selectedUsername: "二狗子"
+    };
   },
-  methods:{
-    onUserChange(event){
-      console.log(event.target.value)
+  methods: {
+    onUserChange(event) {
+      this.$store.commit("setUser", event.target.value);
     }
   }
-}
+};
 </script>
