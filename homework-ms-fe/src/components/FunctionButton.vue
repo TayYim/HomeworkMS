79c74a8e20edd0e146d1a225a0eb0e909e6c5a50
yan_sy <yan_sy@126.com>
2018-10-17 15:26:53 +0800

    init FunctionButton component

---
+++ homework-ms-fe/src/components/FunctionButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="handleClick()">{{ButtonName}}</button>
+        <p v-show="isAuthorized">ok</p>
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "FunctionButton",
+  props: ["ButtonName", "PIDRequired"],
+  methods: {
+    handleClick() {
+      //   alert(this.ButtonName);
+      console.log(this.isAuthorized);
+      if (this.isAuthorized) {
+        alert(this.user.name + "拥有权限，可以操作");
+      } else {
+        alert(this.user.name + "无权限进行此操作");
+      }
+    }
+  },
+  computed: {
+    isAuthorized() {
+      return this.user.PIDs.includes(this.PIDRequired);
+    },
+    ...mapState(["RBAC", "user"])
+  }
+};
+</script>
+
+<style scoped>
+</style>
