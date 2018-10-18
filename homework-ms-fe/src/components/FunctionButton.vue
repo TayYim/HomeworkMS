33406cfa50ca19dcb547fcd896aa085aaea45231
yan_sy <yan_sy@126.com>
2018-10-19 06:59:42 +0800

    add coment

--- homework-ms-fe/src/components/FunctionButton.vue
+++ homework-ms-fe/src/components/FunctionButton.vue
@@ -12,17 +12,24 @@ export default {
   name: "FunctionButton",
   props: ["ButtonName", "PIDRequired"],
   methods: {
+    /**
+     * handleClick()
+     * 本按钮组件的点击事件的事件监听器
+     */
     handleClick() {
-      //   alert(this.ButtonName);
-      console.log(this.isAuthorized);
       if (this.isAuthorized) {
+        // 有权限，通过
         alert(this.user.name + "拥有权限，可以操作");
       } else {
+        // 无权限，拒绝
         alert(this.user.name + "无权限进行此操作");
       }
     }
   },
   computed: {
+    /**
+     * 判断当前用户权限集合是否包含本按钮要求的权限
+     */
     isAuthorized() {
       return this.user.PIDs.includes(this.PIDRequired);
     },
@@ -30,6 +37,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-</style>
